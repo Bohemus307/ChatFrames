1ffc431d97bc5d53534a0a0694951d9a098e9eb6
joshua oxner <joshuaoxner@joshuas-MacBook-Pro.local>
2020-06-26 09:10:58 -0600

    dependency issues

--- src/server/index.js
+++ src/server/index.js
@@ -12,15 +12,12 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 const PORT = config.app.port;
-// app.use(express.static(path.join(__dirname, '/../../lib')));
+app.use(express.static(path.join(__dirname, '/../../lib')));
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 
 //app.use('/data', Router);
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../lib/index.html'));
-});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
-const SRC_DIR = path.join(__dirname, '/src');
+const SRC_DIR = path.join(__dirname, '/src/client');
 const DIST_DIR = path.join(__dirname, '/lib');
 
 module.exports = {
-  entry: `${SRC_DIR}/server/index.js`,
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
