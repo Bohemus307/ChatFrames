c0808d740f19d8f6209888952f00c4ef31e90b2b
joshua oxner <joshuaoxner@joshuas-MacBook-Pro.local>
2020-06-26 09:55:48 -0600

    Commit for a break

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -1,19 +1,46 @@
 import React from 'react';
-import io from "socket.io-client";
+import io from 'socket.io-client';
 
-//const socket = io.connect("http://localhost:5000");
-var socket = io();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (  
-    <div>
-      <ul id="messages"></ul>
-      <form action="">
-        <input id="m" autoComplete="off" /><button>Send</button>
-      </form>
-    </div>
-  );
+    this.state = {
+      endpoint: 'localhost;3030',
+      color: 'white',
+    };
+    this.send = this.send.bind(this);
+    this.setColor = this.setColor.bind(this);
+  }
+
+  componentDidMount() {
+    const socket = io(this.state.endpoint);
+    setInterval(this.send(), 1000);
+    socket.on('change color', (col) => {
+      document.body.style.backgroundColor = col;
+    });
+  }
+
+  setColor(color) {
+    this.setState({ color });
+  }
+
+  send() {
+    const socket = io(this.state.endpoint);
+    socket.emit('change color', this.state.color);
+  }
+
+  render() {
+    const socket = io(this.state.endpoint);
+
+    return (
+      <div style={{ textAlign: "center" }}>
+        <button type="button" onClick={() => this.send()}>Change Color</button>
+        <button type="button" id="blue" onClick={() => this.setColor('blue')}>Blue</button>
+        <button type="button" id="red" onClick={() => this.setColor('red')}>Red</button>
+      </div>
+    );
+  }
 }
-
 
 export default App;
