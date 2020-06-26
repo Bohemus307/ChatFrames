32581f0f3c4b9cce4f6238f92ae85dfe8e4a6b3f
Joshua Scott Oxner <bohemus@hotmail.com>
2020-06-26 09:10:58 -0600

    dependency issues

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import io from "socket.io-client";
 
-<<<<<<< HEAD
 //const socket = io.connect("http://localhost:5000");
-var socket = io.connect();
+var socket = io();
 
 function App() {
   return (  
@@ -17,19 +16,4 @@ function App() {
 }
 
 
-=======
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <ul id="messages"></ul>
-        <form action="">
-          <input id="m" autocomplete="off" /><button>Send</button>
-        </form>
-      </div>
-    );
-  }
-}
-
->>>>>>> 2d440b04a23aceaee656a262f6dc9ab960992780
 export default App;
