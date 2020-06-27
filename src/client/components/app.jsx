87073e35c9848f4e8954b6d675fcef6d092812e1
joshua oxner <joshuaoxner@joshuas-MacBook-Pro.local>
2020-06-27 08:00:58 -0600

    go back to go forward

--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -2,43 +2,9 @@ import React from 'react';
 import io from 'socket.io-client';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      endpoint: 'localhost;3030',
-      color: 'white',
-    };
-    this.send = this.send.bind(this);
-    this.setColor = this.setColor.bind(this);
-  }
-
-  componentDidMount() {
-    const socket = io(this.state.endpoint);
-    setInterval(this.send(), 1000);
-    socket.on('change color', (col) => {
-      document.body.style.backgroundColor = col;
-    });
-  }
-
-  setColor(color) {
-    this.setState({ color });
-  }
-
-  send() {
-    const socket = io(this.state.endpoint);
-    socket.emit('change color', this.state.color);
-  }
-
   render() {
-    const socket = io(this.state.endpoint);
-
     return (
-      <div style={{ textAlign: "center" }}>
-        <button type="button" onClick={() => this.send()}>Change Color</button>
-        <button type="button" id="blue" onClick={() => this.setColor('blue')}>Blue</button>
-        <button type="button" id="red" onClick={() => this.setColor('red')}>Red</button>
-      </div>
+      <div>hello world</div>
     );
   }
 }
--- src/server/index.js
+++ src/server/index.js
@@ -2,14 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors');
-const config = require('../../config');
-
-//const Router = require('./router/router.js');
+const config = require('../../config.js');
 
 const app = express();
 
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+// const Router = require('./router/router.js');
 
 const PORT = config.app.port;
 app.use(express.static(path.join(__dirname, '/../../lib')));
@@ -17,10 +14,6 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 
-//app.use('/data', Router);
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+// app.use('/data', Router);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
