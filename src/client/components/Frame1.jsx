64b92a49960ab4df026b18432ec08e7dabf4b8c6
joshua oxner <bohemus@hotmail.com>
2020-06-30 11:53:26 -0600

    Playing with routers

--- src/client/components/Frame1.jsx
+++ src/client/components/Frame1.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Frame1 = () => (
-  <div>
-    hello frame1
+  <div className="frame1-wrapper">
+    <img className="frame frame1-image" src="/images/frame1.png" alt="frame" />
+    <img className="face" src="/images/face1.png" alt="face" />
   </div>
 );
 
--- src/client/components/app.jsx
+++ src/client/components/app.jsx
@@ -1,22 +1,51 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import Frame1 from './Frame1.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-
-    };
-  }
-
-  render() {
-    return (
+export default function App() {
+  return (
+    <Router>
       <div>
-        <Frame1 />
+        <Link to="/"><Frame1 /></Link>
+
+        <Link to="/about">About</Link>
+
+        <Link to="/users">Users</Link>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return (
+    <div>
+      hello world
+    </div>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
